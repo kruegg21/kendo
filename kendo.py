f291ccc86659f3c3a292186bd21575b9398b232d
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-05-24 17:22:23 -0700

    nested lock capacity added

--- kendo.py
+++ kendo.py
@@ -11,8 +11,13 @@ global_lock = multiprocessing.Lock()
 # very basic process
 # n : process number
 def process(n, lock0, lock1, clocks, lrlt_list, lock_held_list):
-	det_mutex_lock(n, clocks, lock0, lrlt_list[0], lock_held_list[0])
+	det_mutex_lock(n, clocks, lock0, lrlt_list, lock_held_list, 0)
+
+	det_mutex_lock(n, clocks, lock1, lrlt_list, lock_held_list, 1)
+	det_mutex_unlock(n, clocks, lock1, lrlt_list, lock_held_list, 1)
+
 	det_mutex_unlock(n, clocks, lock0, lrlt_list, lock_held_list, 0)
+	clocks[n] = 10000
 ####################################################################################
 
 ####################################################################################
@@ -24,50 +29,88 @@ def process(n, lock0, lock1, clocks, lrlt_list, lock_held_list):
 def wait_for_turn(n, clocks):
 	process_clock_value = clocks[n]
 	while True:
+		# error checking
+		print n, clocks
+
 		if process_clock_value < min(clocks) or \
 		   (process_clock_value == min(clocks) and n == clocks.index(min(clocks))):
-			clocks[n] += 1
 			break
 	return
 
 # ATTEMPTS TO ACQUIRE MUTEX LOCK
 # n : process number
-def det_mutex_lock(n, clocks, lock, lrlt, lock_held):
+def det_mutex_lock(n, clocks, lock, lrlt_list, lock_held_list, lock_number):
 	while True:
 		wait_for_turn(n, clocks)
-		if not lock_held:
-			lock.acquire()
-			if lrlt >= clocks[n]:
+
+		# error checking
+		global_lock.acquire()
+		print "Process", n, "'s Turn with Lock", lock_number
+		print clocks
+		print lrlt_list
+		print '\n'
+		global_lock.release()
+
+		global_lock.acquire()
+		if try_lock(lock, lock_held_list, lock_number):
+			global_lock.release()
+			if lrlt_list[lock_number] >= clocks[n]:
+				# atomically release and label lock as not held
+				global_lock.acquire()
 				lock.release()
+				lock_held_list[lock_number] = 0
+				global_lock.release()
 			else:
 				# error checking
 				global_lock.acquire()
-				print "Process", n, "Locking"
+				print "Process", n, "Locking Lock", lock_number
 				print clocks
 				print '\n'
 				global_lock.release()
 				break
+		else:
+			global_lock.release()
 		clocks[n] += 1
 	clocks[n] += 1
 
 def det_mutex_unlock(n, clocks, lock, lrlt_list, lock_held_list, lock_number):
-	lrlt_list[lock_number] = clocks[n]
+	# atomically release and label lock as not held
+	global_lock.acquire()
 	lock_held_list[lock_number] = 0
+	lrlt_list[lock_number] = clocks[n]
 	lock.release()
 	clocks[n] += 1
 
 	# error checking
-	global_lock.acquire()
-	print "Process", n, "Unlocking"
+	print "Process", n, "Unlocking Lock", lock_number
 	print clocks
+	print lrlt_list[lock_number]
 	print '\n'
 	global_lock.release()
+
+# returns True if lock is free and acquires lock, returns False if lock is not free
+def try_lock(lock, lock_held_list, lock_number):
+	if not lock_held_list[lock_number]:
+		lock_held_list[lock_number] = 1
+		lock.acquire()
+		return True
+	else:
+		return False
+
+def pause_logical_clock():
+	return
+
+def resume_logical_clock():
+	return
+
+def incriment_logical_clock():
+	return 
 ####################################################################################
 
 if __name__ == "__main__":
 	# MAXIMUM NUMBER OF PROCESSES AND NUMBER OF LOCKS
 	# with current code, these values need to be specified
-	max_processes = 5
+	max_processes = 4
 	num_locks = 2
 
 	# CREATE LOCK TO SHARE BETWEEN PROCESS
@@ -82,7 +125,7 @@ if __name__ == "__main__":
 	clocks = manager.list([0] * max_processes)
 
 	# CREATE LIST WITH RELEASE TIME FOR EACH LOCK
-	# lrlt : lock released locical time list
+	# lrlt : lock released logical time list
 	lrlt_list = manager.list([0] * num_locks)
 
 	# CREATE LIST WITH LOCK HELD BOOLEAN
@@ -92,7 +135,7 @@ if __name__ == "__main__":
 	p = range(max_processes)
 	for i in xrange(max_processes):
 		p[i] = multiprocessing.Process(target=process,  
-							    args=(i, lock0, lock1, clocks, lrlt_list, lock_held_list))
+							    	   args=(i, lock0, lock1, clocks, lrlt_list, lock_held_list))
 		p[i].start()
 
 	# WAIT FOR PROCESSES TO END
